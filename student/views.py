c591ea190dc621d24b62b8a41be3d9c6bb555b32
unknown <21bca009@shiats.edu.in>
2023-09-28 03:48:04 +0530

    created seprate urls.py added login logic in views

--- student/views.py
+++ student/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    # user athonticatrion cheak
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'index.html')
+        else:
+            return render(request, 'login.html')
+    return render(request, 'login.html')
